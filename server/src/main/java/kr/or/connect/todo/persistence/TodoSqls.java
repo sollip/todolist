6661182305bdede8ca36c6fe882ff67626f27780
sollip <sollip4293@hanmail.net>
2017-06-06 17:57:09 +0900

    Update TodoSqls.java

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -3,4 +3,12 @@ package kr.or.connect.todo.persistence;
 public class TodoSqls {
 	static final String DELETE_BY_ID =
 			"DELETE FROM todo WHERE id= :id";
+	static final String SELECT_ALL = 
+			"SELECT id, todo, completed, date FROM todo";
+	static final String UPDATE =
+			"UPDATE todo SET completed= :completed WHERE id= :id";
+	static final String SELECT_BY_ID=
+			"SELECT id, todo, completed, date FROM todo WHERE id= :id";
+	static final String DELETE_COMPLETED = 
+			"DELETE FROM todo WHERE completed= :completed";
 }
